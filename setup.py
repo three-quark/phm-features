367b60b7b56efc5d62e2db4a09f9320e49cdbde7
pet <2364839934@qq.com>
2022-07-13 14:37:23 +0800

    add all

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="phm_feature",
+  version="0.0.1",
+  author="qin_hain_ing",
+  author_email="2364839934@qq.com",
+  description="pick feature for phm",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://gitee.com/ultrapower_cd_2364839934_admin/phm-feature",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
